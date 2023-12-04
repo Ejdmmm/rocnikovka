6889036fe4ce9af03f6018ba4e3d75427bf9f4a5
Adam Bartoš <113940525+Ejdmmm@users.noreply.github.com>
2023-12-04 21:39:56 +0100

    Add files via upload

---
+++ hexapod-esp/main.py
@@ -0,0 +1,13 @@
+import config
+
+if config.HTTP_SERVER_ON:
+    import controller
+    controller.start()
+
+
+
+from network import Pin, PWM
+servo = PWM(Pin(13), freq=50)
+servo.duty(20)
+servo.duty(70)
+servo.duty(120)
