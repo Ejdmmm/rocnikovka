c8ad9e29b7892eee605b5a413a495731d02d0212
Adam Bartoš <113940525+Ejdmmm@users.noreply.github.com>
2024-02-01 00:08:00 +0100

    Add files via upload

---
+++ hexapod-esp/motorconf.py
@@ -0,0 +1,7 @@
+RIGHT_FRONT_LEG = "right.front.leg" # prava predni z pohledu predku/horni aby byl cumak pavouka vepredu
+LEFT_FRONT_LEG = "left.front.leg" # leva predni
+MID_RIGHT_LEG = "right.middle.leg" # prava predni
+MID_LEFT_LEG = "left.middle.leg" # leva prostredni
+RIGHT_BACK_LEG = "right.back.leg" # prava zadni
+LEFT_BACK_LEG = "left.back.leg" # leva zadni
+LEFT_MID_BACK_LEG = "left.middle.back.leg" # leva prostredni
